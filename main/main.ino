9e8d196c67c8c874d6c3539e4c80aa9de56a740a
Brian Jacobs <brianjcbs23@gmail.com>
2017-04-22 16:30:21 -0400

    Cleaned up the code

--- main/main.ino
+++ main/main.ino
@@ -1,12 +1,10 @@
-
-//www.elegoo.com
-//2016.12.12
 #include "Stepper.h"
 #include "IRremote.h"
 /*----- Variables, Pins -----*/
 #define STEPS  10   // Number of steps per revolution of Internal shaft
-int  Steps2Take;  // 2048 = 1 Revolution
+#define steps = 20;  // 2048 = 1 Revolution
 int receiver = 12; // Signal Pin of IR receiver to Arduino Digital Pin 6
+
 /*-----( Declare objects )-----*/
 // Setup of proper sequencing for Motor Driver Pins
 // In1, In2, In3, In4 in the sequence 1-3-2-4
@@ -16,41 +14,35 @@ decode_results results;     // create instance of 'decode_results'
 void setup()
 { 
   irrecv.enableIRIn(); // Start the receiver
+  small_stepper.setSpeed(500);
 }
+
 void loop()
 {
-if (irrecv.decode(&results)) // have we received an IR signal?
-  {
-    switch(results.value)
+  if (irrecv.decode(&results)) // have we received an IR signal?
     {
-      case 0xFFA857: // VOL+ button pressed
-                     stepForward();
-      case 0xFF629D: // VOL- button pressed
-                      stepBackward();
-                
-    }
-    
+      switch(results.value)
+      {
+        case 0xFFA857: // VOL+ button pressed
+          stepForward();
+        case 0xFF629D: // VOL- button pressed
+          stepBackward();
+      }
+
       irrecv.resume(); // receive the next value
-                 digitalWrite(8, LOW);
-                 digitalWrite(9, LOW);
-                 digitalWrite(10, LOW);
-                 digitalWrite(11, LOW);       
+        digitalWrite(8, LOW);
+        digitalWrite(9, LOW);
+        digitalWrite(10, LOW);
+        digitalWrite(11, LOW);       
   }  
 }/* --end main loop -- */
+
 void stepForward()
 {
-                  small_stepper.setSpeed(500); //Max seems to be 500
-                      Steps2Take  =  20;  // Rotate CW
-                      small_stepper.step(Steps2Take);
-                      delay(20); 
-                      
+  small_stepper.step(steps);
 }
 void stepBackward()
 {
-                   small_stepper.setSpeed(500);
-                      Steps2Take  =  -20;  // Rotate CCW
-                      small_stepper.step(Steps2Take);
-                      delay(20); 
-                      
+  small_stepper.step(steps);
 }
 
