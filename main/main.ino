a97eca7810c4d08abcde4253316ee32de1940894
Brian Jacobs <brianjcbs23@gmail.com>
2017-04-27 14:28:45 -0400

    Altitude moter works

--- main/main.ino
+++ main/main.ino
@@ -1,32 +1,42 @@
+#include <IRremote.h>
+#include <Stepper.h>
+
 #include "Stepper.h"
 #include "IRremote.h"
 /*----- Variables, Pins -----*/
 #define STEPS  10   // Number of steps per revolution of Internal shaft
-#define steps = 20;  // 2048 = 1 Revolution
+#define s  100  // 2048 = 1 Revolution
+
 int receiver = 12; // Signal Pin of IR receiver to Arduino Digital Pin 6
+int lastCode = 0;
 
 /*-----( Declare objects )-----*/
 // Setup of proper sequencing for Motor Driver Pins
 // In1, In2, In3, In4 in the sequence 1-3-2-4
-Stepper small_stepper(STEPS, 8, 10, 9, 11);
+Stepper altitude(STEPS, 8, 10, 9, 11);
+Stepper azimuth(STEPS, 7, 5, 6, 4);
 IRrecv irrecv(receiver);    // create instance of 'irrecv'
 decode_results results;     // create instance of 'decode_results'
 void setup()
 { 
   irrecv.enableIRIn(); // Start the receiver
-  small_stepper.setSpeed(500);
+  altitude.setSpeed(500);
+  Serial.begin(9600);
 }
 
 void loop()
 {
   if (irrecv.decode(&results)) // have we received an IR signal?
     {
+      Serial.println(results.value);
       switch(results.value)
       {
         case 0xFFA857: // VOL+ button pressed
-          stepForward();
+          stepUp();
+          break;
         case 0xFF629D: // VOL- button pressed
-          stepBackward();
+          stepDown();
+          break;
       }
 
       irrecv.resume(); // receive the next value
@@ -37,12 +47,18 @@ void loop()
   }  
 }/* --end main loop -- */
 
-void stepForward()
+void stepUp()
 {
-  small_stepper.step(steps);
+  altitude.step(s);
 }
-void stepBackward()
+void stepDown()
 {
-  small_stepper.step(steps);
+  altitude.step(-s);
+}
+void stepClockwise(){
+  
+}
+void stepCounterClockwise(){
+  
 }
 
